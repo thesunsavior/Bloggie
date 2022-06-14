a8a31cfa59c2b5573b67e1cab16f4eb89f799e40
Trung <20trung.pq@vinuni.edu.vn>
2022-06-14 10:33:04 +0700

    database

--- bloggie/blog/views.py
+++ bloggie/blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 #some dummy data to load some post
-posts = [
-    {
-        'author': 'dickie',
-        'title': 'my dickie dickie',
-        'content': 'my dickie grew',
-        'date_posted': 'june 15,2022'
-    },
-    {
-        'author': 'Johny deep',
-        'title': 'the starlight of my life',
-        'content': 'starlight sucked me ',
-        'date_posted': 'june 12,2021'
-    },
-]
+# posts = [
+#     {
+#         'author': 'dickie',
+#         'title': 'my dickie dickie',
+#         'content': 'my dickie grew',
+#         'date_posted': 'june 15,2022'
+#     },
+#     {
+#         'author': 'Johny deep',
+#         'title': 'the starlight of my life',
+#         'content': 'starlight sucked me ',
+#         'date_posted': 'june 12,2021'
+#     },
+# ]
 
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all}
 
     return render(request, 'blog/home.html', context)
 
