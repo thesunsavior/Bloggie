81ae6e7b64f876e7f5c4a913139c73caa91dc4bc
Trung <20trung.pq@vinuni.edu.vn>
2022-06-14 08:59:11 +0700

    templates

--- bloggie/blog/views.py
+++ bloggie/blog/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#some dummy data to load some post
+posts = [
+    {
+        'author': 'dickie',
+        'title': 'my dickie dickie',
+        'content': 'my dickie grew',
+        'date_posted': 'june 15,2022'
+    },
+    {
+        'author': 'Johny deep',
+        'title': 'the starlight of my life',
+        'content': 'starlight sucked me ',
+        'date_posted': 'june 12,2021'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {'posts': posts}
+
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
